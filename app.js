a3eb628e991a3b0595421ba3f706e4fbc3762f4d
tonyishangu <tonyishangu@gmail.com>
2023-11-21 00:12:21 +0300

    handling get request

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 // define routes
-app.get('/', (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the backend',
-    app: 'Natours',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
-app.post('/',(req, res) => {
-    // code to handle post request
-    res.status(201).send('You can post to this ui;')
-})
+// app.post('/',(req, res) => {
+//     // code to handle post request
+//     res.status(201).send('You can post to this ui;')
+// })
 
 const port = 3000;
 app.listen(port, () => {
